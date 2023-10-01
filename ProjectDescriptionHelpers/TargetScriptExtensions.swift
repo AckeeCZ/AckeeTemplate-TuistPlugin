2f49af1dd17d25b29d9151765a456b94d233a9c8
Jakub Olejnik <jakub.olejnik@ackee.cz>
2023-10-01 22:28:35 +0200

    ✨ Improve fetching Crashlytics scripts

--- ProjectDescriptionHelpers/TargetScriptExtensions.swift
+++ ProjectDescriptionHelpers/TargetScriptExtensions.swift
@@ -1,33 +1,6 @@
 import ProjectDescription
 
 public extension TargetScript {
-    /// Downloads latest upload dSYM script from Firebase repository and uses it to upload dSYMs
-    static func crashlytics() -> TargetScript {
-        .post(
-            script: """
-            set -e
-            
-            if [ "$CONFIGURATION" != "Debug" ]; then
-                TMPDIR=`mktemp -d`
-            
-                pushd "$TMPDIR"
-                curl "https://raw.githubusercontent.com/firebase/firebase-ios-sdk/master/Crashlytics/run" > run
-                curl "https://raw.githubusercontent.com/firebase/firebase-ios-sdk/master/Crashlytics/upload-symbols" > upload-symbols
-                chmod +x run upload-symbols
-                ./run
-                popd
-                rm -rf "$TMPDIR"
-            fi
-            """,
-            name: "Crashlytics",
-            inputPaths: [
-                "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Resources/DWARF/${TARGET_NAME}",
-                "$(SRCROOT)/$(BUILT_PRODUCTS_DIR)/$(INFOPLIST_PATH)"
-            ],
-            basedOnDependencyAnalysis: false
-        )
-    }
-    
     /// Action that tries to run Swiftlint using mint, or directly if mint is not installed
     ///
     /// If Swiftlint cannot be found, then it does nothing
@@ -35,14 +8,14 @@ public extension TargetScript {
         .post(
             script: """
             export PATH=/opt/homebrew/bin:${PATH}
-
+            
             set -e
-
+            
             if ! [ -f ".swiftlint.yml" ]; then
                 echo "No swiftlint.yml found, not running it"
                 exit 0
             fi
-
+            
             if command -v mint &> /dev/null && grep -iq swiftlint Mintfile; then
                 echo "Mint"
                 pushd "$SRCROOT"
@@ -51,7 +24,7 @@ public extension TargetScript {
                 popd
                 exit 0
             fi
-
+            
             if command -v "swiftlint" &> /dev/null; then
                 echo "Direct"
                 pushd "$SRCROOT"
@@ -60,7 +33,7 @@ public extension TargetScript {
                 popd
                 exit 0
             fi
-
+            
             echo "SwiftLint not found, not running it"
             """,
             name: "SwiftLint",
@@ -69,3 +42,47 @@ public extension TargetScript {
     }
 }
 
+public extension [TargetScript] {
+    /// Downloads latest upload dSYM script from Firebase repository and uses it to upload dSYMs
+    static func crashlytics(
+        outputDir: String = "$SRCROOT/Derived"
+    ) -> [TargetScript] {
+        [
+            .pre(
+                script: """
+                set -e
+                
+                curl "https://raw.githubusercontent.com/firebase/firebase-ios-sdk/master/Crashlytics/run" > \(outputDir)/run
+                curl "https://raw.githubusercontent.com/firebase/firebase-ios-sdk/master/Crashlytics/upload-symbols" > \(outputDir)/upload-symbols
+                chmod +x \(outputDir)/run \(outputDir)/upload-symbols
+                """,
+                name: "Fetch Crashlytics scripts",
+                outputPaths: [
+                    "\(outputDir)/run",
+                    "\(outputDir)/upload-symbols",
+                ]
+            ),
+            .post(
+                script: """
+                set -e
+                
+                "$SCRIPT_INPUT_FILE_0"
+                """,
+                name: "Crashlytics",
+                inputPaths: [
+                    // Input from fetching scripts
+                    "\(outputDir)/run",
+                    "\(outputDir)/upload-symbols",
+                    // Crashlytics files - https://firebase.google.com/docs/crashlytics/get-started?platform=ios#set-up-dsym-uploading
+                    "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}",
+                    "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Resources/DWARF/${PRODUCT_NAME}",
+                    "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Info.plist",
+                    "$(BUILT_PRODUCTS_DIR)/$(UNLOCALIZED_RESOURCES_FOLDER_PATH)/GoogleService-Info.plist",
+                    "$(BUILT_PRODUCTS_DIR)/$(EXECUTABLE_PATH)",
+                    "$(INSTALL_DIR)/$(UNLOCALIZED_RESOURCES_FOLDER_PATH)/GoogleService-Info.plist",
+                ],
+                basedOnDependencyAnalysis: false
+            )
+        ]
+    }
+}
