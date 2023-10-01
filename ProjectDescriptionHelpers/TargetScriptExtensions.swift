8e777cd5037b77ddc579f197140dbb38f4036749
Jakub Olejnik <jakub.olejnik@ackee.cz>
2023-10-01 21:23:21 +0200

    🔥 Remove set build number build phase

--- ProjectDescriptionHelpers/TargetScriptExtensions.swift
+++ ProjectDescriptionHelpers/TargetScriptExtensions.swift
@@ -28,62 +28,6 @@ public extension TargetScript {
         )
     }
     
-    /// Script that reads number of commits in current git branch
-    /// and sets it to Info.plist in target binary (if any) and in Info.plist file
-    ///
-    /// This command should be run only in git repository, otherwise if will fail getting commits count.
-    static func setBuildNumber() -> TargetScript {
-        .post(
-            script: """
-            set -e
-            
-            pushd "$SRCROOT"
-            
-            echo "Updating build number"
-
-            COMMITS=`git rev-list HEAD --count`
-
-            if [ $? -ne 0 ]; then
-                echo "error: Unable to get commits count, make sure you run project from git repo, or comment out this build phase"
-                exit 1
-            else
-                echo "Number of commits $COMMITS"
-            fi
-
-            echo "Updating Info.plist at ${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
-
-            # Update build number in created binary's Info.plist
-            /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $COMMITS" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
-
-            if [ $? -ne 0 ]; then
-                # this write can fail as plist might not be present
-                echo "error: Unable to write binary plist"
-            else
-                echo "Binary plist written"
-            fi
-
-            echo "Updating Info.plist at ${INFOPLIST_FILE}"
-
-            # Update build number in original plist so it is consistent
-            /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $COMMITS" "$INFOPLIST_FILE"
-
-            if [ $? -ne 0 ]; then
-                echo "error: Unable to write local plist"
-                exit 3
-            else
-                echo "Local plist written"
-            fi
-
-            echo "Updating build finished"
-            
-            popd
-            """,
-            name: "Set build number",
-            basedOnDependencyAnalysis: false
-        )
-    }
-
-    
     /// Action that tries to run Swiftlint using mint, or directly if mint is not installed
     ///
     /// If Swiftlint cannot be found, then it does nothing
